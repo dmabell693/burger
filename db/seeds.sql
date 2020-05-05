66cc44a9334de860224b75e3ed5feb7439adac9e
Daniel Abell <da450@cairn.edu>
2020-05-04 20:21:10 -0400

    initial push. added db and config folders, created database and inserted seed values. created server.js file.

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name) VALUES ("bacon cheeseburger");
+INSERT INTO burgers (burger_name) VALUES ("bacon bleu burger");
+INSERT INTO burgers (burger_name) VALUES ("southwestern chipotle");
+
+SELECT * FROM burgers;
